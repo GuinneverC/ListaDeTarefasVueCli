de9828825651b70ee55a95a499d153c14c6f0c9d
GuinneverC <guinnevercorrea.1@gmail.com>
2022-01-22 23:17:35 -0300

    projeto inicial

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container">
+    
+    <h2>{{titulo}}</h2>
+
+    <div class="input-group">
+      <input @keyup.enter="adicionarTarefa" type="text" v-model="novaTarefa">
+      <span class="input-group-btn">
+        <button @click="adicionarTarefa" class="btn btn-primary">Adicionar</button>
+      </span>
+  </div>
+
+ <ul>
+   <li v-for="(tarefa, index) in tarefas" :key="index" :class="{'removed': tarefa.checked}">
+     <input type="checkbox" v-model="tarefa.checked">
+     <label> {{ tarefa.titulo }} </label>
+   </li>
+ </ul>
+
+  <footer>
+    <em>Altere aqui o titulo da sua lista de tarefas</em>
+    <input v-model="titulo" type="text">
+  </footer>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      titulo: "minha lista de tarefas",
+      novaTarefa: '',
+      tarefas: [
+        { titulo:"Estudar", checked: false},
+        { titulo:"Ler", checked: true},
+      ],
+    };
+  },
+  methods: {
+    adicionarTarefa() {
+      this.tarefas.push({
+        titulo: this.novaTarefa,
+        checked: false
+      });
+      this.novaTarefa = "";
+    },
+  },
+};
+</script>
+
+<style>
+.contaner {
+  width: 40%;
+  margin: 20px auto 0px auto;
+}
+ul li {
+list-style: none;
+}
+.removed {
+  color: gray;
+  text-decoration: line-through;
+}
+</style>
